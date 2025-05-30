7901009bc2faafedf89f41576fe2b6694d82255e
QangThun <phamquangthuan3107@gmail.com>
2025-05-30 23:29:58 +0700

    d

--- TX1/de12.cpp
+++ TX1/de12.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 
 using namespace std;
+
 // Cấu trúc máy tính
 struct MayTinh
 {
@@ -14,40 +15,42 @@ struct MayTinh
 
 // DỮ LIỆU CÓ SẴN
 vector<MayTinh> danhSach = {
-    {"MT01", "Nguyen Van A", 60, 30},
-    {"MT02", "Le Thi B", 100, 50},
-    {"MT03", "Tran Van C", 120, 60},
-    {"MT04", "Vo Thi D", 30, 20},
-    {"MT05", "Pham Van E", 70, 30},
-    {"MT06", "Nguyen Van F", 90, 40},
-    {"MT07", "Nguyen Van G", 50, 25}};
-int G = 100; // Tổng số tiền có thể chi
+    {"MT01", "Nguyen Van An", 60, 30},
+    {"MT02", "Le Thi Ban", 100, 50},
+    {"MT03", "Tran Van Cuong", 120, 60},
+    {"MT04", "Vo Thi Duyen", 30, 20},
+    {"MT05", "Pham Van Dong", 70, 30},
+    {"MT06", "Nguyen Van Thanh", 90, 40},
+    {"MT07", "Nguyen Van Giap", 50, 25}};
+int m = 100; // Tổng số tiền có thể chi
 
 // So sánh theo tỉ lệ giá trị / giá thanh lý (Greedy)
-bool soSanhTheoTiSo(const MayTinh &a, const MayTinh &b)
+bool soSanhTheoGiaThanhLy(const MayTinh &a, const MayTinh &b)
 {
-    double tiLeA = (double)a.giaTri / a.giaThanhLy;
-    double tiLeB = (double)b.giaTri / b.giaThanhLy;
-    return tiLeA > tiLeB;
+    return a.giaThanhLy < b.giaThanhLy;
 }
 
 // Thuật toán Tham lam (Greedy)
-void giaiBangGreedy(vector<MayTinh> danhSach, int G)
+void giaiThuatThamLam(vector<MayTinh> danhSach, int m)
 {
-    sort(danhSach.begin(), danhSach.end(), soSanhTheoTiSo);
+    sort(danhSach.begin(), danhSach.end(), soSanhTheoGiaThanhLy);
 
     int tongGiaTri = 0, tongTien = 0;
     vector<MayTinh> ketQua;
 
     for (auto &mt : danhSach)
     {
-        if (tongTien + mt.giaThanhLy <= G)
+        if (tongTien + mt.giaThanhLy <= m)
         {
             tongTien += mt.giaThanhLy;
             tongGiaTri += mt.giaTri;
             ketQua.push_back(mt);
         }
     }
+
+    // Sắp xếp kết quả theo soHieu tăng dần
+    sort(ketQua.begin(), ketQua.end(), [](const MayTinh &a, const MayTinh &b)
+         { return a.soHieu < b.soHieu; });
 
     cout << "\nDanh sach may tinh duoc chon (Thuat toan Tham Lam):\n";
     for (auto &mt : ketQua)
@@ -61,15 +64,15 @@ void giaiBangGreedy(vector<MayTinh> danhSach, int G)
 }
 
 // Quy hoạch động (DP)
-void giaiBangDP(vector<MayTinh> danhSach, int G)
+void giaiThuatQHD(vector<MayTinh> danhSach, int m)
 {
     int n = danhSach.size();
-    vector<vector<int>> dp(n + 1, vector<int>(G + 1, 0));
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
     // Lập bảng DP
     for (int i = 1; i <= n; i++)
     {
-        for (int w = 0; w <= G; w++)
+        for (int w = 0; w <= m; w++)
         {
             if (danhSach[i - 1].giaThanhLy > w)
             {
@@ -87,7 +90,7 @@ void giaiBangDP(vector<MayTinh> danhSach, int G)
     // Truy vết kết quả
     int tongTien = 0;
     vector<MayTinh> ketQua;
-    int w = G;
+    int w = m;
     for (int i = n; i > 0 && w > 0; i--)
     {
         if (dp[i][w] != dp[i - 1][w])
@@ -98,6 +101,10 @@ void giaiBangDP(vector<MayTinh> danhSach, int G)
         }
     }
 
+    // Sắp xếp kết quả theo soHieu tăng dần
+    sort(ketQua.begin(), ketQua.end(), [](const MayTinh &a, const MayTinh &b)
+         { return a.soHieu < b.soHieu; });
+
     cout << "\nDanh sach may tinh duoc chon (Quy hoach dong):\n";
     for (auto &mt : ketQua)
     {
@@ -106,14 +113,14 @@ void giaiBangDP(vector<MayTinh> danhSach, int G)
              << " | Gia tri: " << mt.giaTri
              << " | Gia thanh ly: " << mt.giaThanhLy << endl;
     }
-    cout << "Tong gia tri: " << dp[n][G] << " | Tong tien da dung: " << tongTien << endl;
+    cout << "Tong gia tri: " << dp[n][m] << " | Tong tien da dung: " << tongTien << endl;
 }
 
 int main()
 {
     // Gọi hai phương pháp giải
-    giaiBangGreedy(danhSach, G);
-    giaiBangDP(danhSach, G);
+    giaiThuatThamLam(danhSach, m);
+    giaiThuatQHD(danhSach, m);
 
     return 0;
 }
